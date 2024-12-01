c1c9dad5fde94dfe04b7bd957aa87f79aed7ddf0
solywsh.wang <dont@panic.ltd>
2024-12-02 00:49:12 +0800

    feat: implement basic sftp functionality

--- go.mod
+++ go.mod
@@ -2,6 +2,12 @@ module github.com/solywsh/go-forensic
 
 go 1.23.3
 
-require golang.org/x/crypto v0.29.0
+require (
+	github.com/pkg/sftp v1.13.7
+	golang.org/x/crypto v0.29.0
+)
 
-require golang.org/x/sys v0.27.0 // indirect
+require (
+	github.com/kr/fs v0.1.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+)
---
+++ utils/osx/sftp.go
@@ -0,0 +1,80 @@
+package osx
+
+import (
+	"github.com/pkg/sftp"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+type SftpX struct {
+	*SShX
+	sftpClient *sftp.Client
+}
+
+func (s *SShX) SFTP() *SftpX {
+	if s.client == nil {
+		return nil
+	}
+	sftpClient, err := sftp.NewClient(s.client)
+	if err != nil {
+		// TODO log error
+		return nil
+	}
+	return &SftpX{
+		SShX:       s,
+		sftpClient: sftpClient,
+	}
+}
+
+func (s *SftpX) Close() error {
+	if s.sftpClient == nil {
+		return nil
+	}
+	return s.sftpClient.Close()
+}
+
+func (s *SftpX) ReadDir(path string) ([]os.FileInfo, error) {
+	if s.sftpClient == nil {
+		return nil, nil
+	}
+	return s.sftpClient.ReadDir(path)
+}
+
+func (s *SftpX) Open(path string) (*sftp.File, error) {
+	if s.sftpClient == nil {
+		return nil, nil
+	}
+	return s.sftpClient.Open(path)
+}
+
+func (s *SftpX) Client() *sftp.Client {
+	if s.sftpClient == nil {
+		return nil
+	}
+	return s.sftpClient
+}
+
+func (s *SftpX) DownloadFile(remoteFile, localFile string) error {
+	remoteFileHandle, err := s.sftpClient.Open(remoteFile)
+	if err != nil {
+		return err
+	}
+	defer remoteFileHandle.Close()
+	err = os.MkdirAll(filepath.Dir(localFile), 0755)
+	if err != nil {
+		return err
+	}
+	localFileHandle, err := os.Create(localFile)
+	if err != nil {
+		return err
+	}
+	defer localFileHandle.Close()
+
+	_, err = io.Copy(localFileHandle, remoteFileHandle)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
