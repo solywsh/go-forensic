86fcbde82359fb5fd11a23a708c07867b7168029
solywsh.wang <dont@panic.ltd>
2024-12-06 01:23:45 +0800

    feat(mmkv): use mmkv cgo on darwin

--- db/mmkv.go
+++ db/mmkv.go
@@ -1,33 +1,79 @@
 package db
 
-/*
-#cgo LDFLAGS: -L. -lmmkv
-#include "mmkv.h"
-*/
 import "C"
+import (
+	"encoding/json"
+	"errors"
+	"github.com/ebitengine/purego"
+)
 
-type MmkvCli struct {
-	mmkv C.MMkv
+type MMKV struct {
+	init     func(string)
+	withId   func(string) error
+	getKeys  func() []string
+	getValue func(string) string
+	close    func()
 }
 
-func NewMmkv() *MmkvCli {
-	return &MmkvCli{}
+func NewMMKV() (*MMKV, error) {
+	libc, err := purego.Dlopen("./mmkv/mmkv.dylib", purego.RTLD_NOW|purego.RTLD_GLOBAL)
+	if err != nil {
+		return nil, err
+	}
+	var mk = &MMKV{}
+	var InitializeMMKV func(string)
+	purego.RegisterLibFunc(&InitializeMMKV, libc, "InitializeMMKV")
+	mk.init = InitializeMMKV
+	var MMKVWithID func(string) string
+	purego.RegisterLibFunc(&MMKVWithID, libc, "MMKVWithID")
+	mk.withId = func(id string) error {
+		errStr := MMKVWithID(id)
+		if errStr != "" {
+			return errors.New(errStr)
+		}
+		return nil
+	}
+	var GetAllKeys func() string
+	purego.RegisterLibFunc(&GetAllKeys, libc, "GetAllKeys")
+	mk.getKeys = func() []string {
+		keysStr := GetAllKeys()
+		var keys []string
+		if keysStr != "" {
+			err := json.Unmarshal([]byte(keysStr), &keys)
+			if err != nil {
+				log.Error(err)
+				return nil
+			}
+		}
+		return keys
+	}
+	var GetValue func(string) string
+	purego.RegisterLibFunc(&GetValue, libc, "GetValue")
+	mk.getValue = GetValue
+
+	var Close func()
+	purego.RegisterLibFunc(&Close, libc, "Close")
+	mk.close = Close
+	return mk, nil
 }
 
-func (m *MmkvCli) InitMMKV(dir string) *MmkvCli {
-	C.InitializeMMKV(dir)
+func (m *MMKV) Init(dir string) *MMKV {
+	m.init(dir)
 	return m
 }
 
-func (m *MmkvCli) WithID(filename string) *MmkvCli {
-	m.mmkv = C.MMKVWithID(filename)
-	return m
+func (m *MMKV) WithId(filename string) error {
+	return m.withId(filename)
 }
 
-func (m *MmkvCli) GetString(key string) string {
-	return m.mmkv.GetString(key)
+func (m *MMKV) GetValue(key string) string {
+	return m.getValue(key)
 }
 
-func (m *MmkvCli) AllKeys() []string {
-	return m.mmkv.AllKeys()
+func (m *MMKV) GetKeys() []string {
+	return m.getKeys()
+}
+
+func (m *MMKV) Close() {
+	m.close()
 }
--- db/mmkv_test.go
+++ db/mmkv_test.go
@@ -1,10 +1,22 @@
 package db
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestVersionEmpty(t *testing.T) {
-	t.Log(NewMmkv().
-		InitMMKV(`../testing/db/mmkv/userinfo`).
-		WithID(`userinfo`).
-		AllKeys())
+	mmkv, err := NewMMKV()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = mmkv.Init(`../testing/db/mmkv`).WithId(`userinfo`)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	for _, key := range mmkv.GetKeys() {
+		t.Log(key)
+		t.Log(mmkv.GetValue(key))
+	}
 }
